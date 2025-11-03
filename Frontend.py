eb4bb2a4fae0e28a55bf97eee5b0e325e0637be5
Schmarc <dermarcschenk@gmail.com>
2025-11-03 08:11:49 +0100

    basic frontend

---
+++ Frontend.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.title("Repo Agent📁")
+
+#"AI Key einfügen"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# React to user input
+if prompt := st.chat_input("Put in Link of Repository"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+response = f"Echo: {prompt}"
+# Display assistant response in chat message container
+with st.chat_message("assistant"):
+    st.markdown(response)
+# Add assistant response to chat history
+st.session_state.messages.append({"role": "assistant", "content": response})
+
+"st.session_state object", st.session_state # funktioniert wie dictionary
+
