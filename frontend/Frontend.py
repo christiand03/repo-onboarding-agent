c18aa4dc6588c654406b2381568c0aaf2a5b4e0f
Schmarc <dermarcschenk@gmail.com>
2025-11-06 13:59:07 +0100

    now supports multiple different chats

--- frontend/Frontend.py
+++ frontend/Frontend.py
@@ -1,31 +1,47 @@
 import streamlit as st
 
-st.title("Repo Agent📁")
+# chats initialisierung 
+if "chats" not in st.session_state:
+    st.session_state.chats = {"Chat 1": {"messages": []}}
+if "active_chat" not in st.session_state:
+    st.session_state.active_chat = "Chat 1"
 
-#"AI Key einfügen"
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# sidebar
+if st.sidebar.button("➕ New Chat "):
+    new_name= f"Chat {len(st.session_state.chats)+1}"     
+    st.session_state.chats[new_name] = {"messages": []}
+    st.session_state.active_chat = new_name
+st.sidebar.markdown("---")
+st.sidebar.title("💬 Chats")
+# liste Chats
+for chat_name in st.session_state.chats:
+    if st.sidebar.button(chat_name, key=f"btn_{chat_name}"):
+        st.session_state.active_chat=chat_name
 
+st.sidebar.markdown("---")
+
+st.sidebar.write(f"Active Chat: **{st.session_state.active_chat}**")
+
+
+# aktueller chat
+active = st.session_state.active_chat
+chat= st.session_state.chats[active]
+st.title(st.session_state.active_chat)
+
+
+for message in chat["messages"]:
+   st.chat_message(message["role"]).write(message["content"])
 
 # React to user input
 if prompt := st.chat_input("Put in Link of Repository"):
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    #print message and store it in chats
+    chat["messages"].append({"role":"user", "content":prompt})
+    response = f"Echo: {prompt}" #platzhalter
+    # print response and store it in chats
+    chat["messages"].append({"role":"assistant", "content":prompt})
+    st.rerun()
 
-response = f"Echo: {prompt}"
-# Display assistant response in chat message container
-with st.chat_message("assistant"):
-    st.markdown(response)
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": response})
-
-"st.session_state object", st.session_state # funktioniert wie dictionary
+"st.session_state object:", st.session_state
 
