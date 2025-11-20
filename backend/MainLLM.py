0b3b00bcc6c112f00e8bcd21e3becb6576576f94
Christian <christian.debbertin@gmail.com>
2025-11-20 23:13:43 +0100

    Fixed Errors & Changed LLM Model

--- backend/MainLLM.py
+++ backend/MainLLM.py
@@ -12,7 +12,7 @@ class MainLLM:
     """
     Hauptklasse für die Interaktion mit dem LLM.
     """
-    def __init__(self, api_key: str, prompt_file_path: str, model_name: str = "gemini-flash-latest"):
+    def __init__(self, api_key: str, prompt_file_path: str, model_name: str = "gemini-2.5-pro"):
         if not api_key:
             raise ValueError("Gemini API Key must be set.")
         
@@ -28,7 +28,7 @@ class MainLLM:
             api_key=api_key,
             temperature=1.0, 
         )
-        logging.info(f"LLMHelper initialized with model '{model_name}'.")
+        logging.info(f"Main LLM initialized with model '{model_name}'.")
     
     def call_llm(self, user_input: str):
 
@@ -65,15 +65,15 @@ class MainLLM:
 
 # Für main.py:
 
-    logging.info("Starting Synthesis Phase...")
-    # final_report = main_llm.call_llm(processed_json)
-    logging.info("stream LLM for final report...")
-    full_response = ""
-    for token in main_llm.stream_llm(processed_json):
-    # ------ Statt print den Token ans Frontend schicken ------
-        print(token, end="", flush=True)
-        full_response += token    
-    final_report = full_response
-    logging.info("streaming complete.")
-    logging.info("final report generated.")
-    logging.info("Synthesis Phase completed.")
+    # logging.info("Starting Synthesis Phase...")
+    # # final_report = main_llm.call_llm(processed_json)
+    # logging.info("stream LLM for final report...")
+    # full_response = ""
+    # for token in main_llm.stream_llm(processed_json):
+    # # ------ Statt print den Token ans Frontend schicken ------
+    #     print(token, end="", flush=True)
+    #     full_response += token    
+    # final_report = full_response
+    # logging.info("streaming complete.")
+    # logging.info("final report generated.")
+    # logging.info("Synthesis Phase completed.")
